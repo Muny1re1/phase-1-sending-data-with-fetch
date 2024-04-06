801340d3aa69efda1dbf5bcb40d52c90e076a615
Muny1re1 <munyiriandy@gmail.com>
2024-04-06 17:45:08 +0300

    Fetch send data

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(username, email){
+    
+const secondParameter = {
+    method: "POST",
+    headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name: `${username}`,
+        email: `${email}`,
+    })
+}
+  fetch("http://localhost:3000/users", secondParameter)
+  .then(response => response.json())
+  .then(data => {
+    id = data.id;
+    document.body.append(id);
+  })
+  .catch(error => {document.body.append(error.message)}) 
+}
+submitData("username", "email");
